bd7783d87c0d45565345e564395602f590660efe
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 19:30:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(line)
   if line.length == 0 
     puts "The line is currently empty."
   else 
-    line.each do |name| 
-      puts "The line is currently: #{name}"
+    line.collect do |name| 
+      "The line is currently: #{name}"
   end
 end 
