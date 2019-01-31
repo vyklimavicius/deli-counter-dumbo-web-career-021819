ac1ed1ab79644f1184df3f385f2bdfb8aee79141
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 19:49:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 def line(line)
-  if line.length == 0 
+  new_string = ""
+  if line.length == 0
     puts "The line is currently empty."
-  else 
-    line.collect do |name| 
-      "The line is currently: #{name}"
+  else
+    line.each_with_index do |name,idx|
+      new_string += "#{idx+1}. #{name} "
   end
-end 
+end
+  puts "The line is currently: "  + new_string
+end
+
