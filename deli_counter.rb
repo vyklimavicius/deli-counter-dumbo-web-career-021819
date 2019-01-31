9b097c0e6316011c76c67e1e6acb2d21aa9ba1ed
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 20:24:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,10 @@ def now_serving(line)
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   elsif line.length > 0 
-  
+    line.each_with_index do |name,idx|
+    line.pop()
+    puts "Currently serving #{name}."
+    
 end 
 
 
